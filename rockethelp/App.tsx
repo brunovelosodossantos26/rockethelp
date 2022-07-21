a94f76146bb276e000f1265d75f71fc04d4c1047
Bruno Veloso <brvelosoo@gmail.com>
2022-07-21 15:10:32 -0300

    loading

--- rockethelp/App.tsx
+++ rockethelp/App.tsx
@@ -1,13 +1,19 @@
-import { NativeBaseProvider} from "native-base";
+import { NativeBaseProvider, StatusBar} from "native-base";
 import {THEME}  from './src/styles/theme';
 import { SignIn } from "./src/sreens/SignIn";
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold})
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn/>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
    
   );
---
+++ rockethelp/src/components/Input.tsx
@@ -0,0 +1,26 @@
+import { Input as NativebaseInput, IInputProps } from 'native-base';
+
+export function Input({ ...rest } : IInputProps ) {
+    return (
+        <NativebaseInput
+            bg="gray.700"
+            h={14}
+            size="md"
+            borderWidth={0}
+            fontSize="md"
+            fontFamily="body"
+            color="white"
+            placeholderTextColor="gray.300"
+            _focus={
+                {
+                    borderWidth:1,
+                    borderColor: "green.500",
+                    bg: "gray.700"
+                }
+            }
+            {...rest}
+        />
+
+
+    );
+}
