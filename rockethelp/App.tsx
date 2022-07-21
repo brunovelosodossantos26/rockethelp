e0462dfc48716d82615e853d30de4e2ddbf37eaf
Bruno Veloso <brvelosoo@gmail.com>
2022-07-20 23:20:43 -0300

    fontes

--- rockethelp/App.tsx
+++ rockethelp/App.tsx
@@ -1,9 +1,12 @@
 import { NativeBaseProvider} from "native-base";
+import {THEME}  from './src/styles/theme';
 import { SignIn } from "./src/sreens/SignIn";
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold})
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <SignIn/>
     </NativeBaseProvider>
    
