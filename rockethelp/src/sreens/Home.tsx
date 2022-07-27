de2691404978c91f37c748b2b2909ba4ee43d3d4
Bruno Veloso <brvelosoo@gmail.com>
2022-07-27 16:03:20 -0300

    lista vazia

--- rockethelp/src/sreens/Home.tsx
+++ rockethelp/src/sreens/Home.tsx
@@ -1,19 +1,16 @@
 import { useState } from 'react';
-import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList } from 'native-base';
+import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList, Center, } from 'native-base';
 import Logo from '../assets/logo_secondary.svg';
 import { SignOut } from 'phosphor-react-native'
 import { Filter } from '../components/Filter';
-import { Button} from '../components/Button';
+import { Button } from '../components/Button';
+import {ChatTeardropText} from 'phosphor-react-native'
 import { Order, OrderProps } from '../components/Order';
+import { color } from 'native-base/lib/typescript/theme/styled-system';
 
 export function Home() {
     const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
-    const [orders, setOrders] = useState<OrderProps[]>([{
-        id: '123',
-        patrimony: '123456',
-        when: '18/07/2022 às 10:00',
-        status: 'open'
-    }])
+    const [orders, setOrders] = useState<OrderProps[]>([])
 
     const { colors } = useTheme();
     return (
@@ -60,11 +57,24 @@ export function Home() {
                 </HStack>
 
 
-                <FlatList 
+                <FlatList
                     data={orders}
                     keyExtractor={item => item.id}
-                    renderItem={({item}) => <Order data={item} />}
+                    renderItem={({ item }) => <Order data={item} />}
+                    showsVerticalScrollIndicator= {false}
+                    contentContainerStyle={{paddingBottom: 60}}
+                    ListEmptyComponent={()=>(
+                        <Center>
+                            <ChatTeardropText color={colors.gray[300]} size={40} />
+                            <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                                Você ainda não possui {'\n'}
+                                solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
+                            </Text>
+                        </Center>
+                    )}
                 />
+
+                <Button title="Nova solicitação" />
             </VStack>
         </VStack>
     );
