eb71db3d7f3526d524a48ac49e5998ae0f5e0186
Bruno Veloso <brvelosoo@gmail.com>
2022-07-27 22:59:17 -0300

    Inicio Tela de tarefa

---
+++ rockethelp/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import { HStack } from 'native-base';
+
+export function Header() {
+  return (
+    <HStack
+    w="full"
+    justifyContent="space-between"
+    alignItems="center"
+    bg="gray.600"
+    pb={6}
+    pt={12}>
+
+    </HStack>
+  );
+}
---
+++ rockethelp/src/sreens/Register.tsx
@@ -0,0 +1,9 @@
+import { VStack } from 'native-base';
+
+export function Register() {
+  return (
+    <VStack flex={1} p={6} bg="gray.600">
+
+    </VStack>
+  );
+}
